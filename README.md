0b1f65909343f8260b7b10d36c7f93429406ebbc
LeoBrida <leonardo.brida2000@gmail.com>
2021-12-03 15:55:40 -0300

    Alterações Atributos Privados

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Doubly-Linked-List
 Trabalho 1 de Estrutura de Dados - UFSC
+
+lista duplamente encadeada COM cursor.
+
+As operações públicas que precisam estar implementadas são (no mínimo):
+
+(elemento) acessarAtual()
+(void) InserirAntesDoAtual ( novo )
+(void) InserirApósAtual ( novo )
+(void) inserirNoFim ( novo ) --ok
+(void) inserirNaFrente ( novo ) --ok
+(void) inserirNaPosicao ( k, novo )
+(void) ExcluirAtual ()
+(void) ExcluirPrim ()
+(void) ExcluirUlt ()
+(void) ExcluirElem ( chave )
+(void) ExcluirDaPos ( k )
+(boolean) Buscar ( chave )
+
+As operações para controle do cursor (privadas) devem ser, no mínimo:
+
+avançarKPosições( K )
+retrocederKPosições ( K )
+irParaPrimeiro() --ok
+irParaUltimo() --ok
+
+Outras (de apoio):
+
+(boolean) Vazia()
+(boolean) Cheia()
+(INT) posiçãoDe(chave)
+
+Criem um programa simples que sirva de demonstração básica da sua classe (p.ex. instancie ao menos 1 objeto ListaDuplamenteEncadeada e insira/retire/busque elementos...)
--- doublylinkedlist.py
+++ doublylinkedlist.py
@@ -32,10 +32,41 @@ class No:
     def anterior(self, anterior):
         self.anterior = anterior
     
-class Cursor:
-    def __init__(self, no = None):
-        self.no = no
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.__head = None
+        self.__tail = None
+        self.__tamanho = 0
+        self.__cursor = None
+
+    def getTamanho(self):
+        return self.__tamanho
+
+    def inserirNaFrente(self, elemento):
+        if self.__tamanho == 0:
+            self.__head = No(elemento)
+            self.__tail = self.__head
+            self.__cursor = self.__head
         
+        else:
+            novo_no = No(elemento, self.__head)
+            self.__head.anterior = novo_no
+            self.__head = novo_no
+            self.__cursor.no = self.__head
+        
+        self.__tamanho += 1
+
+    def inserirNoFim(self, elemento):
+        if self.__tamanho == 0:
+            self.inserirNaFrente(elemento)
+        
+        else:
+            novo_no = No(elemento, None, self.__tail)
+            self.__tail = novo_no
+        
+        self.__tamanho += 1
+       
     def __avancarKPosicoes(self, k):
         pass
 
@@ -43,47 +74,42 @@ class Cursor:
         pass
 
     def __irParaPrimeiro(self):
-        pass
+        primeiro = self.__head
+        return primeiro
 
     def __irParaUltimo(self):
-        pass
-
-class DoublyLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-        self.tamanho = 0
-        self.cursor = Cursor()
-
-    def getTamanho(self):
-        return self.tamanho
-
-    def inserirNaFrente(self, elemento):
-        if self.tamanho == 0:
-            self.head = No(elemento)
-            self.tail = self.head
-            self.cursor = self.head
-        
-        else:
-            novo_no = No(elemento, self.head)
-            self.head.anterior = novo_no
-            self.head = novo_no
-            self.cursor.no = self.head
-        
-        self.tamanho += 1
-
-    def inserirNoFim(self, elemento):
-        if self.tamanho == 0:
-            self.inserirNaFrente(elemento)
-        
-        else:
-            novo_no = No(elemento, None, self.tail)
-            self.tail = novo_no
-        
-        self.tamanho += 1
+        ultimo = self.__tail
+        return ultimo
 
     def inserirAntesDoAtual(self):
         pass
 
     def inserirAposAtual(self):
         pass
+
+    def inserirNaPosicao(self):
+        pass
+
+    def ExcluirAtual(self):
+        pass
+
+    def ExcluirPrim(self):
+        pass
+
+    def ExcluirUlt(self):
+        pass
+
+    def ExcluirElem(chave):
+        pass
+
+    def ExlcuirDaPos(k):
+        pass
+    
+    def Buscar(chave):
+        pass
+
+    def Vazia(self):
+        return (self.__head == None and self.__tail == None)
+    
+    def Cheia(self):
+        return (self.__head == None and self.__tail == None)
